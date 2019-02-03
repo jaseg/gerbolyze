b87480afa9fd6b54ff1e523393196640d82518b5
jaseg <git@jaseg.net>
2019-02-03 14:44:43 +0900

    Bump version to v0.1.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name = 'gerbolyze',
-    version = '0.1.7',
+    version = '0.1.8',
     py_modules = ['gerbolyze'],
     scripts = ['gerbolyze'],
     description = ('A high-resolution image-to-PCB converter. Gerbolyze reads and vectorizes black-and-white raster '
