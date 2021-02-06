61e98b439c1d76ff9ee7e536363ff36f66d0e829
jaseg <code@jaseg.net>
2021-02-06 19:27:42 +0100

    Bump version to v2.0.2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ class CustomInstall(install):
 setup(
     cmdclass={'install': CustomInstall},
     name = 'gerbolyze',
-    version = '2.0.0',
+    version = '2.0.2',
     py_modules = ['gerbolyze'],
     package_dir = {'': 'gerbolyze'},
     entry_points = '''
