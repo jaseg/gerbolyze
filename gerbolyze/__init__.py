d8c20e63118f63546d6dffe008cb6b04dc6d6675
jaseg <git@jaseg.de>
2022-06-19 00:51:52 +0200

    convert: generate better file names for weird layers

--- gerbolyze/__init__.py
+++ gerbolyze/__init__.py
@@ -243,6 +243,7 @@ def convert(input_svg, output_gerbers, is_zip, dilate, curve_tolerance, no_subtr
                 trace_space=trace_space, vectorizer=vectorizer, vectorizer_map=vectorizer_map,
                 exclude_groups=exclude_groups, curve_tolerance=curve_tolerance, only_groups=group_id,
                 outline_mode=(use == 'outline' or use == 'drill'))
+        grb.original_path = Path()
 
         if use == 'drill':
             if side == 'plated':
