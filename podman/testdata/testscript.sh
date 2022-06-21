bb8b28f86acd73a5e780765667b7c94f67aaf397
jaseg <git@jaseg.de>
2022-06-21 13:54:29 +0200

    update local tests

--- podman/testdata/testscript.sh
+++ podman/testdata/testscript.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-pip3 install --user /data/gerbolyze-*.tar.gz --no-binary gerbolyze
-/root/.local/bin/svg-flatten --clear-color black --dark-color white --format svg /data/test_svg_readme.svg /out/test_out.svg
+set -e 
+rsync -av /data/git git
+cd git
 
+python3 -m pytest $@
--- run-tests.sh
+++ run-tests.sh
@@ -2,13 +2,32 @@
 
 set -e
 
-python setup.py sdist build
-cp dist/*.tar.gz podman/testdata
+while [ $# -gt 0 ]; do
+    case $1 in
+        --parallel)
+            CONTAINER_ARGS="--workers auto $CONTAINER_ARGS"
+            shift;;
+        -x)
+            CONTAINER_ARGS="-x $CONTAINER_ARGS"
+            shift;;
+        --no-cache)
+            NO_CACHE=--no-cache
+            shift;;
+        *)
+            echo "Unknown argument \"$1\""
+            exit 1
+            shift;;
+    esac
+done
 
-for distro in arch fedora debian ubuntu
+mkdir -p podman/testdata/git
+git ls-tree --full-tree -r HEAD --name-only | rsync -lptgoDv --delete . --files-from - podman/testdata/git/
+#git clone --depth 1 . podman/testdata/git
+
+for distro in ubuntu-old ubuntu arch
 do
-    podman build -t gerbolyze-$distro-testenv -f podman/$distro-testenv
-    mkdir -p /tmp/gerbolyze-test-out
-    podman run --mount type=bind,src=podman/testdata,dst=/data,ro --mount type=bind,src=/tmp/gerbolyze-test-out,dst=/out gerbolyze-$distro-testenv /data/testscript.sh
+    podman build $NO_CACHE -t gerbonara-$distro-testenv -f podman/$distro-testenv
+    mkdir -p /tmp/gerbonara-test-out
+    podman run --mount type=bind,src=podman/testdata,dst=/data,ro --mount type=bind,src=/tmp/gerbonara-test-out,dst=/out gerbonara-$distro-testenv /data/testscript.sh $CONTAINER_ARGS
 done
 
